02385c88048c070ab178315ab8dbb01f9e2b36e4
Guille B <ingmrb@users.noreply.github.com>
2021-01-01 23:58:46 +0100

    Update run_1_query.py

--- run_1_query.py
+++ run_1_query.py
@@ -4,27 +4,34 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import rdflib, urllib
 from rdflib import URIRef
 import os
+from datetime import datetime
+
 
 
 endpointUrl = 'https://query.wikidata.org/sparql'
-
+named_graph_id = 'https://www.wikidata.org'
 
 with open('query.sparql','r') as file:
     query_string = file.read()
 
     print('processing query')
-    itemGraph=rdflib.Graph()
+    itemGraph=rdflib.ConjunctiveGraph(store="IOMemory")
+
+    start_time = datetime.now()
 
     url = endpointUrl + '?query=' + urllib.parse.quote(query_string)
 
     # rdflib retrieves the results, parses the triples, and adds them to the graph
-    result = itemGraph.parse(url)
+    itemGraph.parse(source=url, publicID=named_graph_id)
+
+    print('the query took: ' + str(datetime.now() - start_time))
+
 
     #my_ntriples = result.serialize(format='nt')
-    my_ntriples = result.serialize(format='nt').decode('utf-8')
+    my_ntriples = itemGraph.serialize(format='nquads').decode('utf-8')
 
     print('saving file into: query results')
 
 
-    with open('output.nt', 'w+') as output_file:
+    with open('output.nq', 'w+') as output_file:
         output_file.write(my_ntriples)
